0c2012941f01ac8594b0f74316060a51dcb80888
bartek <nawrockibartoszwro@gmail.com>
2020-03-30 15:55:18 +0200

    modelowanie bazy danych plik models.py

--- wyposazenie/__init__.py
+++ wyposazenie/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-# konfigurcja ścieżki bazy danych
+# konfigurcja sciezki bazy danych
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wyposazenie.db"
 db = SQLAlchemy(app)
 from wyposazenie import routes
