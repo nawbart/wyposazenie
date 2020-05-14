34647d7a8db573842719a8f42cd9acc41453085e
bartek <nawrockibartoszwro@gmail.com>
2020-05-14 21:01:44 +0200

    dodanie app.config[SECRET_KEY]...

--- wyposazenie/__init__.py
+++ wyposazenie/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "ca228fe05da3f26ff064bf69b5960a12"
 # konfigurcja ścieżki bazy danych
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wyposazenie.db"
 db = SQLAlchemy(app)
