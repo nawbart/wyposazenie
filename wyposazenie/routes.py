c11e3cddd5a1afcb4a3505401a4cebc224e47656
bartek <nawrockibartoszwro@gmail.com>
2020-05-25 18:44:06 +0200

    mozliwosc wyswietlania jednego wiersza. Mozliwosc aktualizacji i usuwania wierszy dla tabel osoby i typy_urzadzen

--- wyposazenie/routes.py
+++ wyposazenie/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
 from wyposazenie import app, db
 from wyposazenie.forms import DodajMiejsceForm, DodajOsobeForm, DodajTypUrzadzeniaForm,\
-    DodajUrzadzenieForm, UpdateMiejsceForm
+    DodajUrzadzenieForm, UpdateMiejsceForm, UpdateOsobaForm, UpdateTypUrzadzeniaForm
 from wyposazenie.models import Miejsca, Osoby, TypyUrzadzen, Urzadzenia
 
 
@@ -103,6 +103,13 @@ def pokazosoby():
     osoby_query = Osoby.query.all()
     return render_template('pokaz_osoby.html', osoby = osoby_query)
 
+# READ, wyswietla jeden wybrany rekord z tablicy "osoby"
+@app.route("/pokazosobe/<int:id_osoba>", methods=['GET', 'POST'])
+def pokazosobe(id_osoba):
+    osoba_query = Osoby.query.get_or_404(id_osoba)
+    return render_template("pokaz_osobe.html", osoba = osoba_query )
+
+
 # CREATE, dodaj osobe
 @app.route("/osoba/new", methods=['GET', 'POST'])
 def dodajosobe():
@@ -123,13 +130,66 @@ def dodajosobe():
     return render_template('dodaj_osobe.html', title='Dodaj osobe', form=form,
                            legend="Dodaj osobe")
 
+# UPDATE, aktualizujemy wybrany wiersz z tabeli osoby
+@app.route("/osoba/<int:id_osoba>/update", methods=["GET", "POST"])
+def update_osoba(id_osoba):
+    osoba_query = Osoby.query.get_or_404(id_osoba)
+    form = UpdateOsobaForm()
+    if form.validate_on_submit():
+        #po nacisnieciu przycisku dane z forms.py sa kopiowane do osoba_query
+        # i nastepnie sa commitowane do bazy
+        osoba_query.nazwisko = form.nazwisko.data
+        osoba_query.opis = form.opis.data
+        osoba_query.imie = form.imie.data
+        osoba_query.email = form.email.data
+        db.session.commit()
+        # pokauje komunikat, ze sie udalo
+        flash("Osoba zostala zaktualizowana", "success")
+        #powrot do pokaz_osoba
+        return redirect(url_for("pokazosobe", id_osoba=osoba_query.id_osoba))
+    #jesli chcemy byc pewni ze wypelnienie formy bylo przeprowadzone po żądaniu GET
+    elif request.method == "GET":
+        #te dwie linie wypelniaja formy wybranymi wartosciami z wybranego wiersza
+        form.nazwisko.data = osoba_query.nazwisko
+        form.opis.data = osoba_query.opis
+        form.imie.data = osoba_query.imie
+        form.email.data = osoba_query.email
+    return render_template("dodaj_osobe.html", title="Update Osoby", form=form,
+                           legend="Update Osoby")
+
+# DELETE osoba
+@app.route("/osoba/<int:id_osoba>/delete", methods=["POST"])
+def delete_osoba(id_osoba):
+    #sprawdza czy istnieje pobrany id_osoba w bazie danych, jeśli nie ma to wykonuje sie metoda
+    # .get_or_404(...)
+    osoba_query = Osoby.query.get_or_404(id_osoba, f"Nie ma danego wiersza z id{id_osoba}"
+                                                                f" w tabeli osoby.")
+    #sprawdza czy id_osoba nie bylo wykorzystane w tabeli urzadzenia
+    urzadzenie_query = Urzadzenia.query.filter_by(id_osoba=id_osoba).first()
+    # jesli id_osoba bylo wykorzystane w tabeli urzadzenia to nie mozna usunac wiersza.
+    if urzadzenie_query:
+        flash(f"Ten wiersz nie moze byc usuniety! Zostal uzyty w tabeli urzadzenia,"
+              f" id :{urzadzenie_query.id_osoba}.","danger")
+    # w przeciwnym wypadku usuniecie jest mozliwe:
+    else:
+        db.session.delete(osoba_query)
+        db.session.commit()
+        flash("Osoba zostala usunieta", "success")
+    return redirect(url_for("pokazosoby"))
+
 
 # ========= TYPYURZADZEN ====================================================
-# READ, wyswietla wszystkie rekordy z tabeli "typy urzadzen"
+# READ, wyswietla wszystkie rekordy z tabeli "typy_urzadzen"
 @app.route("/pokaztypyurzadzen")
 def pokaztypyurzadzen():
     typyurzadzen_query = TypyUrzadzen.query.all()
     return render_template('pokaz_typyurzadzen.html', typyurzadzen=typyurzadzen_query)
+
+# READ, wyswietla jeden wybrany rekord z tablicy "typy_urzadzen"
+@app.route("/pokaztypurzadzenia/<int:id_typ_urzadzenia>", methods=['GET', 'POST'])
+def pokaztypurzadzenia(id_typ_urzadzenia):
+    tu_query = TypyUrzadzen.query.get_or_404(id_typ_urzadzenia)
+    return render_template("pokaz_typurzadzenia.html", typurzadzenia = tu_query )
 
 # CREATE, dodaj typy urzadzen
 @app.route("/typurzadzenia/new", methods=['GET', 'POST'])
@@ -149,6 +209,51 @@ def dodajtypurzadzenia():
         return redirect(url_for('pokaztypyurzadzen'))
     return render_template('dodaj_typurzadzenia.html', title='Dodaj typ urzadzenia', form=form,
                            legend="Dodaj typ urzadzenia")
+
+# UPDATE, aktualizujemy wybrany wiersz z tabeli typy_urzadzen
+@app.route("/typurzadzenia/<int:id_typ_urzadzenia>/update", methods=["GET", "POST"])
+def update_typurzadzenia(id_typ_urzadzenia):
+    typurzadzenia_query = TypyUrzadzen.query.get_or_404(id_typ_urzadzenia)
+    form = UpdateTypUrzadzeniaForm()
+    if form.validate_on_submit():
+        #po nacisnieciu przycisku dane z forms.py sa kopiowane do typurzadzenia_query
+        # i nastepnie sa commitowane do bazy
+        typurzadzenia_query.nazwa = form.nazwa.data
+        typurzadzenia_query.opis = form.opis.data
+        db.session.commit()
+        # pokauje komunikat, ze sie udalo
+        flash("typ urzadzenia zostal zaktualizowany", "success")
+        #powrot do pokaz_typurzadzenia
+        return redirect(url_for("pokaztypurzadzenia", id_typ_urzadzenia=typurzadzenia_query.id_typ_urzadzenia))
+    #jesli chcemy byc pewni ze wypelnienie formy bylo przeprowadzone po żądaniu GET
+    elif request.method == "GET":
+        #te dwie linie wypelniaja formy wybranymi wartosciami z wybranego wiersza
+        form.nazwa.data = typurzadzenia_query.nazwa
+        form.opis.data = typurzadzenia_query.opis
+    return render_template("dodaj_typurzadzenia.html", title="Update Typ urzadzenia", form=form,
+                           legend="Update Typ urzadzenia")
+
+# DELETE typ_urzadzenia
+@app.route("/typurzadzenia/<int:id_typ_urzadzenia>/delete", methods=["POST"])
+def delete_typurzadzenia(id_typ_urzadzenia):
+    #sprawdza czy istnieje pobrany id_typ_urzadzenia w tab. typy_urzadzen, jeśli nie ma to wykonuje sie metoda
+    # .get_or_404(...)
+    typurzadzenia_query = TypyUrzadzen.query.get_or_404(id_typ_urzadzenia, f"Nie ma danego wiersza z id{id_typ_urzadzenia}"
+                                                                f" w tabeli osoby.")
+    #sprawdza czy id_typ_urzadzenia nie bylo wykorzystane w tabeli urzadzenia
+    urzadzenie_query = Urzadzenia.query.filter_by(id_typ_urzadzenia=id_typ_urzadzenia).first()
+    # jesli id_typ_urzadzenia bylo wykorzystane w tabeli urzadzenia to nie mozna usunac wiersza.
+    if urzadzenie_query:
+        flash(f"Ten wiersz nie moze byc usuniety! Zostal uzyty w tabeli urzadzenia,"
+              f" id :{urzadzenie_query.id_typ_urzadzenia}.","danger")
+    # w przeciwnym wypadku usuniecie jest mozliwe:
+    else:
+        db.session.delete(typurzadzenia_query)
+        db.session.commit()
+        flash("Typ urzadzdenia zostal usuniety", "success")
+    return redirect(url_for("pokaztypyurzadzen"))
+
+
 
 
 # ========= URZADZENIA ====================================================
